e1aa808dc27e4077d8890faac6a87507f35ffcf5
robinsng <163006854+robinsng@users.noreply.github.com>
2024-03-19 12:21:56 +0100

    content_script.js

--- extension_google/content_script.js
+++ extension_google/content_script.js
@@ -1,14 +1,9 @@
-const assetElement = document.querySelector('[data-role="coin-name"]');
-let assetName = assetElement ? assetElement.getAttribute('title') : null;
-
-if (assetName) {
-  // Remplace les espaces par des tirets
-  assetName = assetName.replace(/\s+/g, '-');
-  
-  // Supprime "Token" si le nom de l'asset se termine par "Token"
-  if (assetName.endsWith('-Token')) {
-    assetName = assetName.substring(0, assetName.length - '-Token'.length);
-  }
-
+const urlElement = document.querySelector('a.chain-name[href*="/token/"]');
+if (urlElement && urlElement.href) {
+  const adresseContrat = new URL(urlElement.href).pathname.split('/').pop();
+  chrome.runtime.sendMessage({ adresseContrat: adresseContrat });
+} else {
+  const assetNameElement = document.querySelector('[data-role="coin-name"]');
+  const assetName = assetNameElement ? assetNameElement.title : "Asset non trouvé";
   chrome.runtime.sendMessage({ assetName: assetName });
 }
